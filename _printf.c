ed05a7ff58f2ed60b0836298b91a674617f42a07
OrionDooms <orion45.dd@gmail.com>
2023-07-23 11:30:25 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * _printf - produces output.
+ * @format: - Takes in a string of character.
+ * Return: length of a string.
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int j, i = 0, len = 0;
+	char *str, c;
+	va_list args;
+
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 's':
+					str = va_arg(args, char*);
+					{
+						for (j = 0; str[j] != '\0'; j++)
+						{
+							putchar(str[j]);
+							len++;
+						}
+					}
+					break;
+				case 'c':
+					c = va_arg(args, int);
+						putchar(c);
+						break;
+				default:
+					break;
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			len++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (len);
+}
